722a53283feba87c7c9260bbcfa29b95f0bb58f4
Abdülkadir <abdulkadiryugruk@gmail.com>
2025-04-25 13:33:17 +0300

    baharatlar duzenlendi

--- src/utils/getImageSource.js
+++ src/utils/getImageSource.js
@@ -1154,70 +1154,39 @@ const imageMappings = {
   'kategoriler/icecekler/salgam/ali-gode-salgam-5-litre-salgam.webp': require(`../../assets/productsImages/kategoriler/icecekler/salgam/ali-gode-salgam-5-litre-salgam.webp`),
 
 
-'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-toz-biber-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-toz-biber-100gr.jpg`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-toz-biber-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-toz-biber-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-urfa-isot-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-urfa-isot-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-anason-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-anason-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-cemen-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-cemen-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-corek-otu-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-corek-otu-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-hashas-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-hashas-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-hindistan-cevizi-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-hindistan-cevizi-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kabuk-tarcin-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kabuk-tarcin-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-karabiber-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-karabiber-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-karbonat-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-karbonat-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kasap-kofte-baharati-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kasap-kofte-baharati-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kimyon-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kimyon-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kisnis-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kisnis-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kori-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kori-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-limon-tuzu-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-limon-tuzu-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-misir-unu-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-misir-unu-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-nane-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-nane-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-pirinc-unu-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-pirinc-unu-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-pul-biber-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-pul-biber-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-sumak-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-sumak-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-susam-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-susam-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-tatli-pul-biber-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-tatli-pul-biber-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-tavuk-cesni-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-tavuk-cesni-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-biber-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-biber-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-sarimsak-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-sarimsak-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-sumak-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-sumak-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-tarcin-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-tarcin-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-yedi-turlu-kofte-baharati-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-yedi-turlu-kofte-baharati-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-zencefil-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-zencefil-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-zerdecal-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-zerdecal-100gr.webp`),
+'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-pul-biber-100gr.webp' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-pul-biber-100gr.webp`),
 
-'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-urfa-isot-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-urfa-isot-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-anason-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-anason-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-cemen-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-cemen-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-corek-otu-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-corek-otu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-hashas-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-hindistan-cevizi-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kabuk-tarcin-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-karabiber-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-karbonat-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kasap-kofte-baharati-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kimyon-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kisnis-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-kori-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-limon-tuzu-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-misir-unu-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-nane-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-pirinc-unu-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-pul-biber-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-sumak-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-susam-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-tatli-pul-biber-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-tavuk-cesni-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-biber-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-sarimsak-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-sumak-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-toz-tarcin-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-yedi-turlu-kofte-baharati-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-zencefil-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-zerdecal-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
-
- 'kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-aci-pul-biber-100gr.jpg' : require(`../../assets/productsImages/kategoriler/temel-gida/tuz-baharat/korkmaz-baharat-galeta-unu-100gr.jpg`),
 };
 
 export const getImageSource = imagePath => {
